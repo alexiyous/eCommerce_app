16258bd9077736983e35b263aa9d4e89d6ef7726
Alexius Andrianno Alfa Renanta <alexiusrenanta@gmail.com>
2025-01-20 13:46:46 +0800

    Initial commit

---
+++ app/src/main/java/com/alexius/shopy/ShopyApp.kt
@@ -0,0 +1,18 @@
+package com.alexius.shopy
+
+import android.app.Application
+import com.alexius.shopy.di.presentationModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class ShopyApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@ShopyApp)
+            modules(listOf(
+                presentationModule,
+            ))
+        }
+    }
+}
